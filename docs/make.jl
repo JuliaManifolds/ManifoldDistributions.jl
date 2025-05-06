b58029efb4b3612b273052818bc6198c6cf65a48
Seth Axen <seth@sethaxen.com>
2025-05-06 22:32:24 +0200

    Run formatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,14 @@
 using ManifoldDistributions
 using Documenter
 
-DocMeta.setdocmeta!(ManifoldDistributions, :DocTestSetup, :(using ManifoldDistributions); recursive=true)
+DocMeta.setdocmeta!(
+    ManifoldDistributions, :DocTestSetup, :(using ManifoldDistributions); recursive=true
+)
 
 makedocs(;
     modules=[ManifoldDistributions],
     authors="Seth Axen <seth@sethaxen.com> and contributors",
     sitename="ManifoldDistributions.jl",
-    format=Documenter.HTML(;
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    format=Documenter.HTML(; edit_link="main", assets=String[]),
+    pages=["Home" => "index.md"],
 )
