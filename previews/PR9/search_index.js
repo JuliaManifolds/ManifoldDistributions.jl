4edfc9eaebb78780737d9ebd0a80b43f358a4e01
Documenter.jl <documenter@juliadocs.github.io>
2025-07-01 06:33:10 +0000

    build based on fd17813

---
+++ previews/PR9/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ManifoldDistributions","page":"Home","title":"ManifoldDistributions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ManifoldDistributions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
